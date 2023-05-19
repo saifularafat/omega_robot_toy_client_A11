a62f5d1684c8e86ca8df8202cfed080c690c84de
saifularafat <saifularafat.info@gmail.com>
2023-05-20 04:01:30 +0600

    sub category secction in car robot done!

--- src/pages/Home/SubCategory/CarRobot/Car.jsx
+++ src/pages/Home/SubCategory/CarRobot/Car.jsx
@@ -1,21 +1,37 @@
+import { Rating } from '@smastrom/react-rating'
+import { Link } from 'react-router-dom';
 
 const Car = ({ car }) => {
     console.log(car);
-    const { img, name, price, Rating } = car
+    const { _id, img, name, price, rating } = car
     return (
         <div>
-            <div className="card w-96 h-fit bg-base-100 shadow-xl">
+            <div
+                data-aos="zoom-in-down"
+                data-aos-duration="1500"
+                className="card w-96 h-fit bg-base-100 shadow-xl">
                 <figure>
-                    <img src={img} alt="robot" className="md:h-72"/>
+                    <img src={img} alt="robot" className="md:h-72" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title sub_title">
                         {name}
                     </h2>
                     <p className="sub_desc"><small>Price: <span className="text-3xl">${price}</span></small></p>
-                    <p className="sub_desc"><small>Rating: {Rating}</small></p>
+                    <div className='flex'>
+                        <p className="sub_desc"><small className=''>Rating:
+                            <Rating
+                                style={{ maxWidth: 25 }}
+                                value={Math.round(rating || 0)}
+                                readOnly
+                            />
+
+                        </small></p>
+                    </div>
                     <div className="text-right">
-                        <button>View Details</button>
+                        <Link to={`/:${_id}`}>
+                            <button className='logout_btn'>View Details</button>
+                        </Link>
                     </div>
                 </div>
             </div>
