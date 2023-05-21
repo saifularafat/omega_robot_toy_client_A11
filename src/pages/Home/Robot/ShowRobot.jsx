c425c31e0e61075e4aae2392b4ffeb56fe6b52ab
saifularafat <saifularafat.info@gmail.com>
2023-05-21 23:59:09 +0600

    Done Alhamdulila

--- src/pages/Home/Robot/ShowRobot.jsx
+++ src/pages/Home/Robot/ShowRobot.jsx
@@ -1,13 +1,12 @@
 import image from './../../../assets/images/about/robot_3.jpg'
 
 const ShowRobot = ({ allToy }) => {
-    console.log(allToy);
     const { img, name } = allToy
     return (
         <div className="mt-8 ">
             <div className="text-center">
                 <div className="avatar">
-                    <div className="md:w-52 md:h-50 rounded">
+                    <div className="w-52 h-50 rounded">
                         <img src={img ? img : image} />
                     </div>
                 </div>
